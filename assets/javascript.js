4cdc0a23b168d2aee1e74f4d3ddfab8c0c9700ec
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-14 13:52:47 +0300

    Create function result

--- assets/javascript.js
+++ assets/javascript.js
@@ -13,13 +13,25 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
 const insertY = ["the soup kitchen", "Disneyland", "the White House"]
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
 
-randomize.addEventListener('click', result);
+randomize.addEventListener("click", result)
 
 function result() {
+  let newStory = storyText
+  let xItem = randomValueFromArray(insertX)
+  let yItem = randomValueFromArray(insertY)
+  let zItem = randomValueFromArray(insertZ)
+  newStory = newStory.replace(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem).replace(":insertx:", xItem)
 
   if (customName.value !== "") {
     const name = customName.value
+    newStory = newStory.replace("Bob", name)
+    console.log(customName2 + "1")
+  }
 
+  if (customName2.value !== "") {
+    const name2 = customName2.value
+    newStory = newStory.replace(":insertx:", name2)
+    console.log(customName2)
   }
 
   if (document.getElementById("est").checked) {
@@ -28,6 +40,6 @@ function result() {
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory
   story.style.visibility = "visible"
 }
